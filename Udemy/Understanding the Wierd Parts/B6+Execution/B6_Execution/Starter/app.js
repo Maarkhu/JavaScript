632776366097bc9d0c6d841d11749ea14264274a
Maarkhu <keramsli@wp.pl>
2022-03-22 16:29:46 +0100

    Execution Stack in JS

--- Udemy/Understanding the Wierd Parts/B6+Execution/B6_Execution/Starter/app.js
+++ Udemy/Understanding the Wierd Parts/B6+Execution/B6_Execution/Starter/app.js
@@ -1,9 +1,15 @@
-b();
+function a() {
+    b();
+    console.log('Called b!');
+}
 
-console.log(a);
+function b() {
+    console.log('Called a!');
+}
+a();
+console.log('Called c!');
 
 var a = 'Hello World!';
 
-function b() {
-    console.log('Called b!');
-}
+console.log(a);
+console.log('Do you see this Execution Stack?')
