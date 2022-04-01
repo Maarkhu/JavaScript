a935c0ad7030f9528dd986c1745c21945daa5e75
Maarkhu <keramsli@wp.pl>
2022-04-01 02:44:14 +0200

    Trochę zmian, praca w VSC

---
+++ MegaK/Part 1 - JavaScript/Etap 1/W2/index.js
@@ -0,0 +1,14 @@
+console.log('Hello')
+
+class Imiona {
+    constructor(marek, zegarek) {
+        this.marek = marek;
+        this.zegarek = zegarek;
+    }
+}
+const pierdziarko = new Imiona('kwiaty', 'bratki')
+console.log(pierdziarko)
+pierdziarko['zegarek'] = ['roze', 'fafki'];
+console.log(pierdziarko)
+const zabarko = new Imiona('piłka', 'kulka')
+console.log(zabarko);
--- Udemy/Understanding the Wierd Parts/B3+Global+Environment/B3_GlobalEnvironment/Starter/app.js
+++ Udemy/Understanding the Wierd Parts/B3+Global+Environment/B3_GlobalEnvironment/Starter/app.js
@@ -1,5 +1,37 @@
-var a = 'Hello Word!';
+//Wiek
+const num = prompt('Wpisz swoje imię.').trim();
+const sur = prompt('Wpisz swoje nazwisko.').trim();
+const age = prompt('Proszę wpisz swój wiek.').trim();
 
-function b() {
+if (age < 18) {
+    console.log('Proszę opuść tę stronę. Przeczytaj książkę, pograj w piłkę z kolegami, łotever.')
+};
 
+if (age >= 18) {
+    console.log(num + ' ' + sur + ' Jesteś zwycięzcą!')
+    console.log('W nagrodę możesz skorzystać z naszego nowoczesnego kalkulatora dla dorosłych!')
+    //kalkulator
+    let nu1 = Number(prompt('Podaj pierwszą liczbę'));
+    for (nu1; isNaN(nu1); nu1 = Number(prompt('Podaj ponownie pierwszą liczbę.'))) {
+        console.log('Nie wpisałeś cyfry, spróbuj jeszcze raz.')
+    }
+    let nu2 = Number(prompt('A teraz drugą'));
+    for (nu1; isNaN(nu2); nu2 = Number(prompt('Podaj drugą liczbę, tylko się znowu nie pomyl, bo pętla się nie skończy:)'))) {
+        console.log('Nie wpisałeś cyfry, spróbuj jeszcze raz')
+    }
+    const sign = prompt('Podaj znak działania');
+    if (sign === '-') {
+        console.log(nu1 - nu2);
+    } else
+    if (sign === '+') {
+        console.log(nu1 + nu2);
+    } else
+    if (sign === '*') {
+        console.log(nu1 * nu2);
+    } else
+    if (sign === '/' && nu2 !== 0) {
+        console.log(nu1 / nu2)
+    } else {
+        console.log('Nie rób tego, nie dziel przez zero.')
+    }
 }
