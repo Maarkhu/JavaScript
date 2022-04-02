d1ed109cba4062e8d83cbbcbf6675171d5a3a865
Maarkhu <keramsli@wp.pl>
2022-04-02 22:34:22 +0200

    Pop Arrays - fuction returns filtered array

--- MegaK/Part 1 - JavaScript/Etap 1/W2/cities.js
+++ MegaK/Part 1 - JavaScript/Etap 1/W2/cities.js
@@ -1,3 +1,5 @@
 const cities = ['Wrocław', 'Poznań', 'Kraków', 'Warszawa', 'Katowice', 'Bytom', 'Jelenia Góra', 'Jastrzębie-Zdrój', 'Rabka-Zdrój'];
-const filteredCities = cities.filter(str => !(str.length % 2)).map(str => str.toUpperCase());
+const filteredCities = cities
+    .filter(str => !(str.length % 2))
+    .map(str => str.toUpperCase());
 console.log(filteredCities);
