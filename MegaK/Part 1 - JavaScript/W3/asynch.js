157099cc584166cf9cad8a5bd660ffdf1f1abb4c
Maarkhu <keramsli@wp.pl>
2022-04-04 13:12:28 +0200

    Asynch

--- MegaK/Part 1 - JavaScript/W3/asynch.js
+++ MegaK/Part 1 - JavaScript/W3/asynch.js
@@ -1,7 +1,21 @@
-console.log('Witaj');
+// console.log('Witaj');
+
+// setTimeout(() => {
+//     console.log('Hello Word!')
+// }, 1000);
+
+// setInterval(() => {
+//     console.log('Hello Marek!')
+// }, 1000);
+
+// console.log('Żegnaj');
+
 
 setTimeout(() => {
     console.log('Hello Word!')
 }, 1000);
 
-console.log('Żegnaj')
+for (let i = 0; i < 10000; i++) {
+    console.log(i);
+}
+console.log('Halo??');
