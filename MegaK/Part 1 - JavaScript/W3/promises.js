ade97bb173f2fd657256bccbe0664075869ac648
Maarkhu <keramsli@wp.pl>
2022-04-06 23:19:38 +0200

    Zad async

--- MegaK/Part 1 - JavaScript/W3/promises.js
+++ MegaK/Part 1 - JavaScript/W3/promises.js
@@ -4,15 +4,5 @@ fetch(`https:www.googleapis.com/books/v1/volumes?q=isbn:${bookNum}`)
     .then((response) => response.json())
     .then((data) => {
         console.log(` Autor: ${data.items[0].volumeInfo.authors[0]}, Tytuł: ${data.items[0].volumeInfo.title}`)
-        // Rozwiązanie Kuby:
-        // if (!data.items || data.items.length === 0) {
-        //     console.log('Book not found!');
-        // } else {
-        //     const {
-        //         authors,
-        //         title
-        //     } = data.items[0].volumeInfo
-        // console.log(`${title} by ${authors}`);
-        // }
     })
     .catch((err) => console.log('Coś jest nie tak', err))
