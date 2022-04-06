2864379049659bbbb080666cb90b26b4576ffbe3
Maarkhu <keramsli@wp.pl>
2022-04-06 15:22:32 +0200

    Promisy i fetch

--- MegaK/Part 1 - JavaScript/W2/cities.js
+++ MegaK/Part 1 - JavaScript/W2/cities.js
@@ -1,5 +1,13 @@
-const cities = ['Wrocław', 'Poznań', 'Kraków', 'Warszawa', 'Katowice', 'Bytom', 'Jelenia Góra', 'Jastrzębie-Zdrój', 'Rabka-Zdrój'];
-const filteredCities = cities
-    .filter(str => !(str.length % 2))
-    .map(str => str.toUpperCase());
-console.log(filteredCities);
+// const cities = ['Wrocław', 'Poznań', 'Kraków', 'Warszawa', 'Katowice', 'Bytom', 'Jelenia Góra', 'Jastrzębie-Zdrój', 'Rabka-Zdrój'];
+// const filteredCities = cities
+//     .filter(str => !(str.length % 2))
+//     .map(str => str.toUpperCase());
+// console.log(filteredCities);
+
+const book = prompt("Wybierz ksiązkę podając liczbę z przedziału 0 - 9");
+
+fetch(`https://www.googleapis.com/books/v1/volumes?q=isbn:`)
+    .then(response => response.json())
+    .then(data => console.log(`Wybrałeś książkę o tytule:
+     ${data.items[book].volumeInfo.title} autora ${data.items[book].volumeInfo.authors}`))
+    .catch(err => console.log(`Coś poszłonie tak i nie wiem jeszcze co`));
