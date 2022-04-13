77f919c0da28a0af1fcfdc3658de53f055340f64
Maarkhu <keramsli@wp.pl>
2022-04-13 22:17:42 +0200

    If statements

--- Udemy/Javascript-course-master/01-Fundamentals-Part-1/starter/challenges.js
+++ Udemy/Javascript-course-master/01-Fundamentals-Part-1/starter/challenges.js
@@ -25,15 +25,35 @@
 // #Challenge 2
 // some code from #Challenge 1
 
-const markMass = 78;
-const markHeight = 1.69;
-const johnMass = 92;
-const johnHeight = 1.95;
-let markBMI = markMass / markHeight ** 2;
-let johnBMI = johnMass / (johnHeight * johnHeight);
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+// let markBMI = markMass / markHeight ** 2;
+// let johnBMI = johnMass / (johnHeight * johnHeight);
 
-if (markBMI > johnBMI) {
-    console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
-} else {
-    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
-};
+// if (markBMI > johnBMI) {
+//     console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+// } else {
+//     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+// };
+
+// #Challenge 3
+
+const doSc1 = 97;
+const doSc2 = 112;
+const doSc3 = 101;
+const koSc1 = 109;
+const koSc2 = 95;
+const koSc3 = 106;
+
+const avDoSc = (doSc1 + doSc2 + doSc3) / 3;
+const avKoSc = (koSc1 + koSc2 + koSc3) / 3;
+console.log(avDoSc.toFixed(1), avKoSc.toFixed(1));
+if (avDoSc > avKoSc) {
+    console.log(`Dolphins won the competition. They have ${avDoSc.toFixed(1)} points!`)
+} else if (avDoSc < avKoSc) {
+    console.log(`Koalas won the competition. They have ${avKoSc.toFixed(1)} points!`)
+} else if (avDoSc == avKoSc) {
+    console.log(`Ladies and Gentelman there is a draw!`)
+}
