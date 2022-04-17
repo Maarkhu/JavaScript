8a99627ca4e7b5c765b8757fc31e319147414c43
Maarkhu <keramsli@wp.pl>
2022-04-17 22:22:28 +0200

    Basket proj cz 1

---
+++ MegaK/Part 1 - JavaScript/WEEK 4/Project-basket.js
@@ -0,0 +1,50 @@
+class Basket {
+    constructor() {
+        this.items = [];
+        this.totalValue = 0;
+    }
+    add (item) {
+        this.items.push(item);
+        this.addToTotalValue(item.price)
+    }
+    addToTotalValue (newPrice) {
+        this.totalValue += newPrice
+    }
+    getTotalValue () {
+       return this.items.reduce((prev, curr) => prev + curr.price, 0);
+    }
+    showBasket () {
+        this.items
+            .map((product, i) =>
+                `${i+1} - ${product.name} - ${product.price.toFixed(2)}zł.`)
+            .forEach(line => console.log(line))
+    }
+    remove(no){
+      this.items.splice(no - 1, 1);
+    }
+}
+
+class Product {
+    constructor(productName, productPrice) {
+        this.name = productName;
+            this.price = productPrice;
+    }
+    setNewPrice (newPrice) {
+        this.price = newPrice;
+    }
+}
+const shopBasket = new Basket();
+const oranges = new Product('Pomarańcze LUZ', 7.55);
+console.log(oranges);
+const cucumbers = new Product('Ogórki', 9.50);
+shopBasket.add(cucumbers);
+shopBasket.add(cucumbers);
+shopBasket.add(oranges);
+console.log(shopBasket);
+console.log(shopBasket.getTotalValue());
+shopBasket.remove(2);
+shopBasket.showBasket();
+
+
+
+
