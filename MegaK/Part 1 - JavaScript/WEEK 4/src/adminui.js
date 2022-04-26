7a8b9c2c579cc66ecc250b3404c9f339d7eeb8dc
Maarkhu <keramsli@wp.pl>
2022-04-26 21:11:04 +0200

    Basket proj cz 4 end.

--- MegaK/Part 1 - JavaScript/WEEK 4/src/adminui.js
+++ MegaK/Part 1 - JavaScript/WEEK 4/src/adminui.js
@@ -1,8 +1,35 @@
 const addProductForm = document.querySelector('.form-add-product');
-const nameInput = document.querySelector('[name="product-name"]');
+const nameInput = document.querySelector('[name="product-name"] ');
 const priceInput = document.querySelector('[name="product-price"]');
+const listaProd = document.querySelector('.lista-prod');
 
-addProductForm.addEventListener('submit', event => {
+const addProductToShop = event => {
     event.preventDefault();
-    console.log('Haloo...?', event)
-} )
+    // console.log('Haloo...?', event);
+    // console.log('Marek', event.target.elements['product-name'].value);
+    console.log(nameInput.value)
+    const name = nameInput.value;
+    const price = Number(priceInput.value);
+
+    const newLi = document.createElement('li'); // <li>
+    const newStrong = document.createElement('strong');
+    newStrong.innerText = name;
+
+    const newPriceText = document.createTextNode(` - ${price.toFixed(2)}`);
+
+    const newBtn = document.createElement('button');
+    newBtn.innerText = 'Kup!'
+    newBtn.classList.add('btn-buy-product');
+    newBtn.dataset.name = name;
+    newBtn.dataset.price = price;
+    newBtn.addEventListener('click', addProductToBasket);
+
+    newLi.appendChild(newStrong);
+    newLi.appendChild(newPriceText);
+    newLi.appendChild(newBtn);
+
+
+    listaProd.appendChild(newLi)
+}
+
+addProductForm.addEventListener('submit', addProductToShop);
