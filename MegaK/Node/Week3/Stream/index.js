8c2f6c9e17cf528ffa317b99eb33f2912b56ddd3
Maarkhu <keramsli@wp.pl>
2022-09-08 23:35:17 +0200

    Read and write stream.

--- MegaK/Node/Week3/Stream/index.js
+++ MegaK/Node/Week3/Stream/index.js
@@ -9,3 +9,17 @@ const {pipeline} = require('stream').promises;
  )
   console.log('Done!');
 })();
+
+// Niepromisowe
+// const r = createReadStream('tree.webp');
+// const w = createWriteStream('tree2.webp');
+//
+// r.pipe(w);
+//
+//   r.on('end', () => console.log('Ready'));
+// Niepromisowe dokładne
+// r.on('data', data => w.write(data));
+// r.on('end', () => {
+//   w.close();
+//   console.log('Ready');
+// })
