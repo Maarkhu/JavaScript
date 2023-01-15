cb5f6a4dc217e58373abfb7b8b941c364ca568e6
Maarkhu <keramsli@wp.pl>
2023-01-15 12:27:45 +0100

    Express - methods and homework - Day2.

--- MegaK/Express.js/W1/D1-D2/zadanie.js
+++ MegaK/Express.js/W1/D1-D2/zadanie.js
@@ -5,7 +5,9 @@ const app = express();
 app.get ('/', async (req, res) => {
   console.log('<h1>Hello from backend</h1>')
   const html = await readFile('./zad.html', 'utf8');
-  res.write(`${html}`);
-  res.end('<h1>No co tam</h1>');
+  const stat = req.header('User-Agent');
+  console.log(stat);
+  await res.send(`<h1>User agent to:<br> ${stat}.<br> Ale pięknie.</h1>`);
+  await res.end();
 });
 app.listen(3000, 'localhost');
